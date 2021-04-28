f58dead968c234c59d684a9d7e336213e827ae1a
Hapsarip <73091334+Hapsarip@users.noreply.github.com>
2021-04-29 01:53:50 +0700

    Add files via upload

---
+++ ConnectDatabase/ConnectDatabase/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConnectDatabase
+{
+    class Connection
+    {
+        public SqlConnection GetConnection()
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source = HAPSARI; initial catalog = Menu; integrated security = true";
+            return Conn;
+        }
+    }
+}
